84a982d5bc8b0923333b746c3eca1fdf69b5d56e
Olga <101519229+Olga2502@users.noreply.github.com>
2023-02-13 22:21:01 +0300

    Решение 13 задачи

---
+++ EX13/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a < 0)
+{
+    a = a*-1;
+}
+   
+if (a > 100)
+{
+   while (a > 999) 
+   {
+    a = a / 10; 
+   }
+   int a1 = a % 10;
+   Console.WriteLine(a1);
+}
+else 
+Console.WriteLine($"Число {a} двухзначное , третье цифра отсутвует");
+
+
+
+
